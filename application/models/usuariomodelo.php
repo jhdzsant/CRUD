d708adfec0729833ae656b8a98705ae4ca5b152e
jask8xd <jask8_xd@hotmail.com>
2015-01-29 14:19:25 -0600

    Actualizacion modelo usuario.php
    Se cambiaron variables para consultar la tabla de users

--- application/models/usuariomodelo.php
+++ application/models/usuariomodelo.php
@@ -6,38 +6,33 @@ class UsuarioModelo extends CI_Model{
     }
 
     function getUsuarios(){
-        $where = array(
-            "activo" => 1
-        );
+
         $this->db->select('*');
         $this->db->from('usuario');
-        $this->db->where($where);
         $respuesta = $this->db->result();
         return encode_json($respuesta);
     }
 
     function addUsuario($email, $password){
         $insert = array(
-            "email"          => $email,
+            "email"    => $email,
             "password" => $password
         );
         $this->db->insert('users', $insert);
         return true;
     }
 
-    function updUsuario($nombre, $apellidopaterno, $usuario, $contraseña, $id_usuario){
+    function updUsuario($email, $password, $id){
         $upd = array(
-            "nombre"          => $nombre,
-            "apellidopaterno" => $apellidopaterno,
-            "usuario"         => $usuario,
-            "contraseña"      => $contraseña
+            "email"    => $email,
+            "password" => $password
         );
-        $this->db->where('id_usuario',$id_usuario);
-        $this->db->update('usuario', $upd);
+        $this->db->where('id',$id);
+        $this->db->update('users', $upd);
         return true;
     }
 
-    function delUsuario($id_usuario){
+    function delUsuario($id){
         $upd = array(
             "activo" => 0
         );
